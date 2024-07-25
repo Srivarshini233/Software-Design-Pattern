b45800ba6c000c5416a4dfd52ea97ce44f64df3d
Srivarshini233 <srivarshinir23@gmail.com>
2024-07-25 19:22:51 +0530

    Login,signup

--- Day3/vite-project/src/pages/Web/Home.jsx
+++ Day3/vite-project/src/pages/Web/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='h-full w-full flex justify-center items-center'>
+      <h1>Home</h1>
+    </div>
   )
 }
 
